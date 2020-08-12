b293792aa7a0d30729a6e78fa26d1a02301d2780
hz-qq2 <601661706@qq.com>
2020-08-12 20:26:52 +0800

    express 与 sequelize-demo 桥接好，整个从数据库连接到express操作数据库过程做好

--- sequelize-demo/src/app.js
+++ sequelize-demo/src/app.js
@@ -1,11 +1,37 @@
 const  express = require('express') ;
+const models = require('../models')
 
 
 const app = express();
 
-app.get('/test', (req,res, next)=>{
+app.get('/create', async (req,res, next)=>{
+    const {name} = req.query;
+    const user = await models.User.create({
+        name
+    })
     res.json({
-        msg:'test ',
+        msg:'create success!',
+    })
+})
+
+
+app.get('/list', async (req,res, next)=>{
+    const {name} = req.query;
+    const list = await models.User.findAll();
+    res.json({
+        list
+    })
+})
+
+
+app.get('/detail/:id', async (req,res, next)=>{
+    const {id} = req.params;
+
+    const detail = await models.User.findOne({
+        where:{id}
+    });
+    res.json({
+        detail
     })
 })
 
