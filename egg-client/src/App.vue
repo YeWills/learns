fd2cfbf1fc437592004754ffae4019087a157588
hz-qq2 <601661706@qq.com>
2020-08-15 10:18:36 +0800

    egg-vue-client 初始化能运行ok

---
+++ egg-client/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <van-button type="primary">主要按钮</van-button>
+  </div>
+</template>
+
+<script>
+import {Button} from 'vant'
+
+export default {
+  name: 'App',
+  components: {
+    [Button.name]:Button
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
