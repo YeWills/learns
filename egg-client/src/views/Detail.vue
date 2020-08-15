ca1a32c88566cae14bac213edeb4d1a967bf0539
hz-qq2 <601661706@qq.com>
2020-08-15 11:45:45 +0800

    egg-vue-client 前端页面ok

--- egg-client/src/views/Detail.vue
+++ egg-client/src/views/Detail.vue
@@ -1,13 +1,34 @@
 <template>
-    <div>
-        detail
-    </div>
+  <div>
+    <van-panel :title="detail.title">
+      <div>{{detail.content}}</div>
+      <template #footer>{{detail.createTime}}</template>
+    </van-panel>
+  </div>
 </template>
 <script>
+import { Panel } from "vant";
+import axios from "axios";
+import moment from "moment";
 export default {
-    
-}
+  name: "Detail",
+  data() {
+    return {
+      detail: {}
+    };
+  },
+  components: {
+    [Panel.name]: Panel
+  },
+  created() {
+    axios
+      .get("http://localhost:7001/article/" + this.$route.query.id)
+      .then(res => {
+        res.data.msg.createTime = moment(res.data.msg.createTime).format("YYYY-MM-DD HH:mm:ss");
+        this.detail = res.data.msg;
+      });
+  }
+};
 </script>
 <style scoped>
-
 </style>
--- egg-client/src/views/Home.vue
+++ egg-client/src/views/Home.vue
@@ -1,13 +1,71 @@
 <template>
-    <div>
-        home
-    </div>
+  <div>
+    <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+      <van-cell v-for="item in list" :key="item.id" @click="clickEvent(item.id)">
+        <div class="a-img">
+          <img :src="item.img" />
+        </div>
+        <div class="a-text">
+          <div>{{item.title}}</div>
+          <div>{{item.createTime}}</div>
+        </div>
+      </van-cell>
+    </van-list>
+  </div>
 </template>
 <script>
+import { List, Cell } from "vant";
+import axios from "axios";
+import moment from "moment";
 export default {
-    
-}
+  components: {
+    [List.name]: List,
+    [Cell.name]: Cell
+  },
+  data() {
+    return {
+      list: [],
+      loading: false,
+      finished: false
+    };
+  },
+  methods: {
+    clickEvent(id) {
+      this.$router.push({
+        path: "detail",
+        query: {
+          id
+        }
+      });
+    },
+    onLoad() {
+      axios.get("http://localhost:7001/article").then(res => {
+        if (res.status == 200) {
+          this.loading = false;
+          this.finished = true;
+          //对每个文章的时间进行格式化
+          this.list = res.data.msg.map(item => {
+            item.createTime = moment(item.createTime).format(
+              "YYYY-MM-DD HH:mm:ss"
+            );
+            return item;
+          });
+        }
+      });
+    }
+  },
+  watch: {
+    list(){
+      this.onLoad();
+    }
+  },
+};
 </script>
 <style scoped>
-
+.a-img {
+  float: left;
+}
+.a-text {
+  float: right;
+}
 </style>
