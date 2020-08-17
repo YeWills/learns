854f835d2892fe5e2eca4c29a3102779b8475bef
hz-qq2 <601661706@qq.com>
2020-08-17 23:02:57 +0800

    egg入门

--- egg-client/vue.config.js
+++ egg-client/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+    // publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
     devServer: {
         proxy: { // 配置代理信息
             '/article': {
