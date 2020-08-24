6c66641598cbb687dfa95ab1c4e3b352a510c97c
hz-qq2 <601661706@qq.com>
2020-08-25 01:26:42 +0800

    websocket

--- node/websocket/websocket.js
+++ node/websocket/websocket.js
@@ -56,3 +56,23 @@ wss.on('connection', function (ws) {
 
    
 });
+
+
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path')
+
+//http://127.0.0.1:3000/icon.jpg
+//http://127.0.0.1:3000/test.html
+const port = 3000;
+http.createServer((req,res)=>{
+    const file = req.url.replace('/','') || 'test.html'
+    fs.readFile(path.resolve(__dirname,file), (err, data)=>{
+        if(err){
+            res.writeHead(404);
+            res.end('404 not found')
+        }
+        res.end(data)
+    })
+}).listen(port, ()=>console.log(`open port ${port}`))
